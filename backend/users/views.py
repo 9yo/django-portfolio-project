f9113c1f17437f31e942ba39225a15f2f3b79677
Artem <abogdaov@gmail.com>
2022-06-08 12:59:00 +0300

    operations on users permission changed from IsAuthenticated to IsAdminUser

--- backend/users/views.py
+++ backend/users/views.py
@@ -11,7 +11,7 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
 
 class CreateUserView(CreateAPIView):
